4f47c64c0becbc9b40bf0a717add35b2cf1edd43
RaduKostashchuk <kostasc@mail.ru>
2021-11-25 17:16:34 +0700

    1. Visibility. Общий ресурс вне критической секции [#1102 #232614]

--- src/main/java/ru/job4j/concurrent/io/ParseFile.java
+++ src/main/java/ru/job4j/concurrent/io/ParseFile.java
@@ -19,18 +19,18 @@ public final class ParseFile {
     }
 
     private String content(Predicate<Character> filter) {
-        String output = "";
+        StringBuilder output = new StringBuilder();
         try (BufferedInputStream in =
                      new BufferedInputStream(new FileInputStream(file))) {
             int data;
             while ((data = in.read()) > 0) {
                 if (filter.test((char) data)) {
-                    output += (char) data;
+                    output.append((char) data);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return output;
+        return output.toString();
     }
 }
