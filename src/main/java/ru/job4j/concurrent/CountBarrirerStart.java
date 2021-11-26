b08742777dedbe3a5b87224a819e32c0090c8ad9
RaduKostashchuk <kostasc@mail.ru>
2021-11-26 10:47:15 +0700

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/concurrent/CountBarrirerStart.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+public class CountBarrirerStart {
+    public static void main(String[] args) {
+        CountBarrier cb = new CountBarrier(3);
+        Thread thr1 = new Thread(
+                () -> {
+                    cb.await();
+                    System.out.println("thr1 started");
+                }
+        );
+        Thread thr2 = new Thread(
+                () -> {
+                    cb.await();
+                    System.out.println("thr2 started");
+                }
+        );
+        Thread thr3 = new Thread(
+                () -> {
+                    try {
+                        cb.count();
+                        System.out.println("Ready");
+                        Thread.sleep(1000);
+                        cb.count();
+                        System.out.println("Steady");
+                        Thread.sleep(1000);
+                        System.out.println("Go!");
+                        cb.count();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        thr1.start();
+        thr2.start();
+        thr3.start();
+    }
+}
