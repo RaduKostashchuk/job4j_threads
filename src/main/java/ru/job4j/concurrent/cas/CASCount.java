f15812ba68a7b624b511e47c1b8a9d6c596f9bd3
RaduKostashchuk <kostasc@mail.ru>
2021-11-27 20:40:50 +0700

    0. CAS - операции [#6859 #233245]

--- src/main/java/ru/job4j/concurrent/cas/CASCount.java
+++ src/main/java/ru/job4j/concurrent/cas/CASCount.java
@@ -9,10 +9,10 @@ public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
-        int temp;
+        Integer temp;
         do {
             temp = count.get();
-        } while (!count.compareAndSet(temp, ++temp));
+        } while (!count.compareAndSet(temp, temp + 1));
     }
 
     public int get() {
--- src/test/java/ru/job4j/concurrent/cas/CASCountTest.java
+++ src/test/java/ru/job4j/concurrent/cas/CASCountTest.java
@@ -7,13 +7,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CASCountTest {
 
+    private static class Increment implements Runnable {
+        private final CASCount count;
+
+        public Increment(CASCount count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 200; i++) {
+                count.increment();
+                System.out.println(Thread.currentThread().getName() + " " + count.get());
+            }
+        }
+    }
+
     @Test
-    public void whenIncrement() {
+    public void whenIncrement() throws InterruptedException {
         CASCount count = new CASCount();
-        count.increment();
-        count.increment();
-        count.increment();
-        assertThat(count.get(), is(3));
+        Thread first = new Thread(
+                new Increment(count), "first");
+        Thread second = new Thread(
+                new Increment(count), "second");
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get(), is(400));
     }
 
 }
