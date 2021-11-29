e8cf569de252ac24eeec887cc6384cf95955435b
RaduKostashchuk <kostasc@mail.ru>
2021-11-29 13:47:44 +0700

    2. ExecutorService рассылка почты. [#63097]

--- src/main/java/ru/job4j/concurrent/mailing/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/mailing/EmailNotification.java
@@ -4,11 +4,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    private final ExecutorService pool;
-
-    public EmailNotification() {
-        this.pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    }
+    private final ExecutorService pool =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = "Notification " + user.getUsername() + " to email " + user.getEmail();
