241c17ed70edad99f6197cbdfabba70bc312ffdb
RaduKostashchuk <kostasc@mail.ru>
2021-12-09 13:05:07 +0700

    3.4. Временное хранение. git stash. [#309409]

--- src/main/java/ru/job4j/git/Hello2.java
+++ src/main/java/ru/job4j/git/Hello2.java
@@ -3,5 +3,6 @@ package ru.job4j.git;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
     }
 }
