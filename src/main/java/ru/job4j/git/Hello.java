04140de466f2e0bfffc7da213b07ab2039d10c70
RaduKostashchuk <kostasc@mail.ru>
2021-12-09 12:16:11 +0700

    3.2. Откат изменений. [#307144]

--- src/main/java/ru/job4j/git/Hello.java
+++ src/main/java/ru/job4j/git/Hello.java
@@ -3,5 +3,8 @@ package ru.job4j.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(Math.random());
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(System.currentTimeMillis());
     }
 }
